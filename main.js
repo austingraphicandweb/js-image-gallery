671ab0f875e11d1d2562a93fae8376bad66fbc8d
Austin Keener <austingraphicandweb@gmail.com>
2020-11-17 13:19:58 -0500

    opacity change fix

--- main.js
+++ main.js
@@ -4,18 +4,24 @@ const current = document.querySelector("#current");
 const imgs = document.querySelectorAll(".imgs img");
 const opacity = 0.4;
 
-//image click event
-imgs.forEach((img) =>
-  img.addEventListener("click", imgClick));
+imgs[0].style.opacity = opacity;
 
 function imgClick(e) {
+  //resetting the opacity
+  imgs.forEach((img) => (img.style.opacity = 1));
 
-    //resetting the opacity
-    imgs.forEach(img => (img.style.opacity = 1));
+  //change image source
+  current.src = e.target.src;
 
-    //change image source
-    current.src = e.target.src;
+  // change the opacity
+  e.target.style.opacity = opacity;
+  
+  //fade in class
+current.classList.add('fade-in');
 
-    // change the opacity
-    e.target.style.opacity = opacity;
+//removing the fade-in class
+setTimeout(() => current.classList.remove('fade-in'), 500);
 };
+
+//image click event
+imgs.forEach((img) => img.addEventListener('click', imgClick));
